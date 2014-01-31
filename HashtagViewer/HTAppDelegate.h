4890ee25af0b2e3d680b75f6c625b033923752aa
strelok <mkuznetsov@onet.ru>
2014-02-01 00:43:04 +0400

    Initial Commit

---
+++ HashtagViewer/HTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HTAppDelegate.h
+//  HashtagViewer
+//
+//  Created by Mikhail Kuznetsov on 01.02.14.
+//  Copyright (c) 2014 mkuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HashtagViewer/HTViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTViewController.h
+//  HashtagViewer
+//
+//  Created by Mikhail Kuznetsov on 01.02.14.
+//  Copyright (c) 2014 mkuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTViewController : UIViewController
+
+@end
